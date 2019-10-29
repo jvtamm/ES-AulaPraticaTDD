40a90933e5b5dc38dbdfcdef12a01c615328ab3a
jvtamm <jv_tamm@hotmail.com>
2019-10-29 20:34:48 -0300

    COMMIT #3

--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -3,11 +3,12 @@ import org.junit.runner.JUnitCore;
 
 public class MoneyTest extends TestCase {
     public void testEquality() {
-       assertTrue(new Dollar(5).equals(new Dollar(5)));
-       assertFalse(new Dollar(5).equals(new Dollar(6)));
-       assertTrue(new Franc(5).equals(new Franc(5)));
-       assertFalse(new Franc(5).equals(new Franc(6)));
-    }
+		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+		assertTrue(Money.franc(5).equals(Money.franc(5)));
+		assertFalse(Money.franc(5).equals(Money.franc(6)));
+		assertFalse(Money.franc(5).equals(Money.dollar(5)));
+	}
   
     public static void main(String[] args) throws Exception  {
 		JUnitCore.main("MoneyTest");
