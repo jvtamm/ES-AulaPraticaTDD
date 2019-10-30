0f976114bffe8d5d21ef179a590e3270636004a4
jvtamm <jv_tamm@hotmail.com>
2019-10-29 21:04:36 -0300

    COMMIT #5

--- src/Money.java
+++ src/Money.java
@@ -1,28 +1,30 @@
-public abstract class Money {
+public class Money {
 	protected int amount;
-  	protected String currency;
-  
-  	Money(int amount, String currency) {
+	private String currency;
+
+	Money(int amount, String currency) {
 		this.amount = amount;
 		this.currency = currency;
 	}
-  
-  	static Money dollar(int amount) {
-		return new Dollar(amount, "USD");
+
+	static Money dollar(int amount) {
+		return new Money(amount, "USD");
 	}
-  
-  	static Money franc(int amount) {
-		return new Franc(amount, "CHF");
+
+	static Money franc(int amount) {
+		return new Money(amount, "CHF");
 	}
-  
-  	abstract Money times(int multiplier); 
-  	
-  	String currency() {
-		return currency;
+
+	Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
 	}
 
 	public boolean equals(Object object) {
 		Money money = (Money) object;
-		return amount == money.amount && getClass().equals(money.getClass());
-   }
+		return amount == money.amount && currency().equals(money.currency());
+	}
+
+	String currency() {
+		return currency;
+	}
 }
